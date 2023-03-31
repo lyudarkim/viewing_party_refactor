9c79811a191bd2f3313688c0c346ac70c6f10ae7
Lyuda Kim <lyudarkim@gmail.com>
2023-03-31 11:52:23 -0700

    Wrote Movie class (part 2) and movie class test.

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,11 @@
 import pytest
+from viewing_party.movie import Movie
 
-def test_1():
-    # Arrange
-
-    # Act
-
+def test_returns_valid_attributes():
+    # Arrange/ Act
+    movie = Movie("Ada Lovelace", "documentary", 5)
+    
     # Assert
-    pass
+    assert movie.title == "Ada Lovelace"
+    assert movie.genre == "documentary"
+    assert movie.rating == 5
